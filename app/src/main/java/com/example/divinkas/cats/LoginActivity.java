cb7c6280ff6d20013b751d307b31de8ec94bdfc7
divinkas <divinkas@gmail.com>
2018-09-24 22:03:23 +0300

    Refactoring code. login activity

--- app/src/main/java/com/example/divinkas/cats/LoginActivity.java
+++ app/src/main/java/com/example/divinkas/cats/LoginActivity.java
@@ -24,16 +24,18 @@ import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.concurrent.Executor;
 
-public class LoginActivity extends MvpAppCompatActivity implements IloginView, View.OnClickListener {
+public class LoginActivity extends MvpAppCompatActivity implements IloginView {
 
     private static final int RC_SIGN_IN = 228;
-    private FirebaseAuth mAuth;
+    private FirebaseAuth firebaseAuth;
     private GoogleSignInClient mGoogleSignInClient;
 
 
-    private EditText etLogin, etPass;
-    private Button btnLogin, btnReg;
-    private SignInButton btnSingIn;
+    private EditText etLogin, etPassword;
+    private Button btnLogin, btnOpenRegistrationView;
+    private SignInButton btnSingInWithGoogle;
+
+    private View.OnClickListener login_and_openRegistration_listener;
 
     @InjectPresenter
     public LoginPresenter loginPresenter;
@@ -44,24 +46,39 @@ public class LoginActivity extends MvpAppCompatActivity implements IloginView, V
         setContentView(R.layout.activity_login);
 
         etLogin = findViewById(R.id.etLogin);
-        etPass = findViewById(R.id.etPass);
+        etPassword = findViewById(R.id.etPass);
         btnLogin = findViewById(R.id.btnSingIn);
-        btnReg = findViewById(R.id.btnReg);
-        btnSingIn = findViewById(R.id.btnSingInGoogle);
+        btnOpenRegistrationView = findViewById(R.id.btnReg);
+        btnSingInWithGoogle = findViewById(R.id.btnSingInGoogle);
 
-        btnLogin.setOnClickListener(this);
-        btnReg.setOnClickListener(this);
-        btnSingIn.setOnClickListener(this);
+        btnLogin.setOnClickListener(login_and_openRegistration_listener);
+        btnOpenRegistrationView.setOnClickListener(login_and_openRegistration_listener);
+        btnSingInWithGoogle.setOnClickListener(login_and_openRegistration_listener);
 
-        mAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-    }
+        login_and_openRegistration_listener = v -> {
+            switch (v.getId()){
+                case R.id.btnSingIn:
+                    mailSingIn();
+                    break;
+                case R.id.btnReg:
+                    Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+                case R.id.btnSingInGoogle:
+                    signIn();
+                    break;
+            }
+        };
 
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -85,7 +102,7 @@ public class LoginActivity extends MvpAppCompatActivity implements IloginView, V
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
         AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
+        firebaseAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
                         setContentView(R.layout.activity_main); // it's progress bar
@@ -95,28 +112,11 @@ public class LoginActivity extends MvpAppCompatActivity implements IloginView, V
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btnSingIn:
-                mailSingIn();
-                break;
-            case R.id.btnReg:
-                Intent intent = new Intent(this, RegistrationActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            case R.id.btnSingInGoogle:
-                signIn();
-                break;
-        }
-    }
-
-    @Override
     public void mailSingIn() {
-        if(!etLogin.getText().toString().isEmpty() && !etPass.getText().toString().isEmpty()) {
+        if(!etLogin.getText().toString().isEmpty() && !etPassword.getText().toString().isEmpty()) {
             String email = etLogin.getText().toString();
-            String password = etPass.getText().toString();
-            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener((Executor) this, task -> {
+            String password = etPassword.getText().toString();
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener((Executor) this, task -> {
                 if(task.isSuccessful()) {
                     gotoCatsActivity();
                 }
